0cac0d83ea5fb41c977ecdc1c0e005efa60a6bad
ExceptionDev <cristiandavdsaenzgamboa@gmail.com>
2024-01-27 15:38:07 -0500

    nuevas funcionalidades y mejora de interfaz

--- app.js
+++ app.js
@@ -1,34 +1,65 @@
 
 const values = {
-    a : 'ai',
-    e : 'enter',
-    i : 'imes',
-    o : 'ober',
-    u : 'ufat'
+    a: 'ai',
+    e: 'enter',
+    i: 'imes',
+    o: 'ober',
+    u: 'ufat'
 }
 
 
 function encrypt() {
     var message = document.getElementById('message').value;
     var encryptedMessage = "";
-
-    for (var character of message) {
-        if (character in values) {
-            encryptedMessage = encryptedMessage.concat(values[character]);
-        } else {
-            encryptedMessage = encryptedMessage.concat(character);
+    if (message != "") {
+        for (var character of message) {
+            if (character in values) {
+                encryptedMessage = encryptedMessage.concat(values[character]);
+            } else {
+                encryptedMessage = encryptedMessage.concat(character);
+            }
         }
+        var encryptedArea = document.getElementById('textEncrypted');
+        encryptedArea.value = encryptedMessage;
+        showText();
     }
-    var encryptedArea = document.getElementById('encrypted');
-    encryptedArea.value = encryptedMessage;
-
 }
 
-function decrypt(){
+function decrypt() {
     let encryptedMessage = document.getElementById('message').value;
     let message = '';
 
-    for (let caracter of encryptedMessage){
-        
+    for (let caracter of encryptedMessage) {
+
+    }
+}
+function showText() {
+    let image = document.querySelector('#imagenLupa');
+    let paragraph = document.querySelector('.encryptParagraph');
+    let paragraph2 = document.querySelector('.encryptParagraph2');
+    let textEncrypted = document.querySelector('#textEncrypted');
+    let button = document.querySelector('.copy');
+    if (textEncrypted.style.display === 'none') {
+        textEncrypted.style.display = 'inline-block';
+        image.style.display = 'none';
+        paragraph.style.display = 'none';
+        paragraph2.style.display = 'none';
+        button.style.display = 'inline-block'
+    }
+
+}
+function copyText() {
+    try {
+        let text = document.getElementById('textEncrypted');
+        text.select();
+        navigator.clipboard.writeText(text.value)
+            .then(() => {
+                alert('¡Texto copiado al portapapeles!');
+            })
+            .catch(err => {
+                alert('Error al intentar copiar el texto: ' + err);
+            });
+    } catch (error) {
+        console.error('Error al intentar copiar el texto: ', error);
     }
 }
